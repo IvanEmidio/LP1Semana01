3a83f07270c4ff8ed1597afcd0a0d6b5c58028bf
Ivan <ivan.emidio@live.com.pt>
2024-02-22 22:51:45 +0000

    Alteraçoes comentarios Ex2

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,15 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// programa feito na aula 1
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// ao correr abre uma mensagem
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
