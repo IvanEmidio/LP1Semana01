9bd7abd9ae7cbe2f00462cbb2875902bbe07f554
Ivan <ivan.emidio@live.com.pt>
2024-02-22 23:13:40 +0000

    numeros inteiros

---
+++ VariousTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace VariousTypes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte sb = 1;
+            short st = 2;
+            int inteiro = 4;
+            long lg = 300L;
+            byte bite = 4;
+            ushort ust = 60;
+            uint uinteiro = 8U;
+            ulong ulg = 700UL;
+            char caracter = 'a';
+
+            Console.WriteLine(sb);
+            Console.WriteLine(st);
+            Console.WriteLine(inteiro);
+            Console.WriteLine(lg);
+            Console.WriteLine(bite);
+            Console.WriteLine(ust);
+            Console.WriteLine(uinteiro);
+            Console.WriteLine(ulg);
+            Console.WriteLine(caracter);
+        }
+    }
+}
