4a8ff491a260b5d941833d39748e8fa04a91272c
IvanEmidio <ivan.emidio@live.com.pt>
2024-02-23 14:21:25 +0000

    Exercicio 7 completo Strings

--- AskForInput/Program.cs
+++ AskForInput/Program.cs
@@ -6,7 +6,15 @@ namespace AskForInput
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Escolha um numero inteiro: ");
+            string escolha = Console.ReadLine();
+            Console.WriteLine("Escolha um numero real: ");
+            string escolha1 = Console.ReadLine();
+
+            int numero = int.Parse(escolha);
+            float numero1 = float.Parse(escolha1);
+
+            Console.WriteLine($"A soma de ambos os numeros é: {numero + numero1}");
         }
     }
 }
