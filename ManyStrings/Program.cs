c52b448f0b5c596023e98d6cd4c009bfa0f49bef
Ivan <ivan.emidio@live.com.pt>
2024-02-23 02:02:20 +0000

    Algumas string unicode e escape

---
+++ ManyStrings/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ManyStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string plica = "isto é uma plica - \'";
+            string copy = "isto é um copyright - \u03A9";
+            string back = "isto é para um back space - \b";
+
+
+
+            Console.WriteLine(plica);
+            Console.WriteLine(copy);
+            Console.WriteLine(back);
+        }
+    }
+}
