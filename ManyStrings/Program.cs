1e1b9b45b5f3c53eb17d8eb738d1f9243b968813
Ivan <ivan.emidio@live.com.pt>
2024-02-23 02:13:29 +0000

    adiçao de string com concatenaçao e interpolaçao

--- ManyStrings/Program.cs
+++ ManyStrings/Program.cs
@@ -12,9 +12,23 @@ namespace ManyStrings
 
 
 
+
+
             Console.WriteLine(plica);
             Console.WriteLine(copy);
             Console.WriteLine(back);
+
+            string primeira = "dia ";
+            int num = 10;
+            string segunda = " vou sair.";
+            int num2 = 5;
+
+            ///concatenaçao
+            Console.WriteLine(primeira + num + segunda);
+            Console.WriteLine("hoje vou comprar "+ num2 + " cartas para o meu baralho.");
+            ///interpolaçao 
+            Console.WriteLine($"a soma de {num} mais {num2} vai dar {num + num2}");
+            Console.WriteLine($"a subtraçao de {num} menos {num2} vai dar {num - num2}");
         }
     }
 }
