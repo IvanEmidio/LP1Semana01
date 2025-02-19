397dc0f6eb235863619d194c37664d8104c565a4
IvanEmidio <ivan.emidio@live.com.pt>
2025-02-19 15:38:54 +0000

    Added comments

--- MeuPrograma/Program.cs
+++ MeuPrograma/Program.cs
@@ -2,8 +2,17 @@
 
 namespace MeuPrograma
 {
+    /// <summary>
+    /// Class starts here
+    /// </summary>
     public class Program
     {
+        /// <summary>
+        /// Program starts here.
+        /// </summary>
+        /// <param name="args">
+        /// Parameters end
+        /// </param>
         private static void Main(string[] args)
         {
             Console.WriteLine("Hello LP1!");
