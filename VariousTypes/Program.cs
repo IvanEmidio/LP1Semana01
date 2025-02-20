10bf285b993097b6685d298ff285b067943a32f8
Ivan <ivan.emidio@live.com.pt>
2025-02-20 20:39:08 +0000

    Added variaveis reais com os literais

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -4,12 +4,17 @@ namespace VariousTypes
 {
     public class Program
     {
-        int num = 1;
-        uint _num = 2U;
-        long _num1 = 3L;
-        ulong _num2 = 3UL;
-        char _backspace = '\u0008'
-        char _tab = '\u0009'
+        static int num = 1;
+        static uint _num = 2U;
+        static long _num1 = 3L;
+        static ulong _num2 = 3UL;
+        static char _backspace = '\u0008';
+        static char _tab = '\u0009';
+
+        static float _float = 3.2f;
+        static double _double = 3.2345;
+
+        static decimal _decimal = 4.436534m;
 
         private static void Main(string[] args)
         {
@@ -20,6 +25,10 @@ namespace VariousTypes
 
             Console.WriteLine($"Isto é um character com Unicode: {_backspace}");
             Console.WriteLine($"Isto é um character com Unicode: {_tab}");
+
+            Console.WriteLine($"Isto é um float com o seu literal: {_float}");
+            Console.WriteLine($"Isto é um Double: {_double}");
+            Console.WriteLine($"Isto é um Decimal com o seu literal: {_decimal}");
 
 
         }
