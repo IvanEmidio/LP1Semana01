9b00e4ece6c231d09252af71fefde4dcc87d65df
Ivan <ivan.emidio@live.com.pt>
2025-02-20 20:21:00 +0000

    Added chars

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -8,12 +8,19 @@ namespace VariousTypes
         uint _num = 2U;
         long _num1 = 3L;
         ulong _num2 = 3UL;
+        char _backspace = '\u0008'
+        char _tab = '\u0009'
+
         private static void Main(string[] args)
         {
             Console.WriteLine($"Isto é um inteiro normal: {num}");
             Console.WriteLine($"Isto é um inteiro com literal uint: {_num}");
             Console.WriteLine($"Isto é um inteiro com literal long: {_num1}");
             Console.WriteLine($"Isto é um inteiro com literal ulong: {_num2}");
+
+            Console.WriteLine($"Isto é um character com Unicode: {_backspace}");
+            Console.WriteLine($"Isto é um character com Unicode: {_tab}");
+
 
         }
     }
