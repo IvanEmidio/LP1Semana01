76f8f67d7ef3027ba7c7f91b19f67cc767e031e8
Ivan <ivan.emidio@live.com.pt>
2025-02-20 21:35:24 +0000

    Added Exercise 7

---
+++ AskForInput/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AskForInput
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Insira um numero inteiro: ");
+            string _int = Console.ReadLine();
+            int a = int.Parse(_int);
+            Console.WriteLine("Insira um numero real: ");
+            string _float = Console.ReadLine();
+            float b = float.Parse(_float);
+            Console.WriteLine($"A soma dos 2 valores é {a+b}");
+        }
+    }
+}
