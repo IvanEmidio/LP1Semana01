8f265dda0fe783a19d19be61412ab2766abd18bd
Ivan <ivan.emidio@live.com.pt>
2025-02-20 20:50:46 +0000

    updated Varioustypes exercise

--- VariousTypes/Program.cs
+++ VariousTypes/Program.cs
@@ -22,6 +22,21 @@ namespace VariousTypes
 
         private static void Main(string[] args)
         {
+            int num = 1;
+            uint _num = 2U;
+            long _num1 = 3L;
+            ulong _num2 = 3UL;
+            char _backspace = '\u0008';
+            char _tab = '\u0009';
+
+            float _float = 3.2f;
+            double _double = 3.2345;
+            decimal _decimal = 4.436534m;
+
+            bool _bool = true;
+            bool _bool2 = false;
+
+
             Console.WriteLine($"Isto é um inteiro normal: {num}");
             Console.WriteLine($"Isto é um inteiro com literal uint: {_num}");
             Console.WriteLine($"Isto é um inteiro com literal long: {_num1}");
@@ -36,7 +51,6 @@ namespace VariousTypes
 
             Console.WriteLine($"Isto é uma variavel bool: {_bool}");
             Console.WriteLine($"Isto é outra variavel bool: {_bool2}");
-
 
         }
     }
