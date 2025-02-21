aceddd61ef0b0d1026c90ed2c558b1a48b3bceb6
IvanEmidio <ivan.emidio@live.com.pt>
2025-02-21 14:06:59 +0000

    Added Exercise Percentagem made in class

---
+++ Percentagem/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+namespace Percentagem
+{
+    public class Program
+    {
+        private static void Main(string[] args)
+        {
+            CultureInfo.DefaultThreadCurrentCulture = CultureInfo.CurrentCulture;
+            Console.WriteLine("Insere 4 números reais entre 0 e 1: ");
+            string num1 = Console.ReadLine();
+            string num2 = Console.ReadLine();
+            string num3 = Console.ReadLine();
+            string num4 = Console.ReadLine();
+            float _num1 = float.Parse(num1);
+            float _num2 = float.Parse(num2);
+            float _num3 = float.Parse(num3);
+            float _num4 = float.Parse(num4);
+            Console.WriteLine($"{_num1:p2}");
+            Console.WriteLine($"{_num2:p2}");
+            Console.WriteLine($"{_num3:p2}");
+            Console.WriteLine($"{_num4:p2}");
+
+        }
+    }
+}
